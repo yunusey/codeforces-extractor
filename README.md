38057d67a8e4b108f2c6b6696e1b732b71614e7d
yunusey <107340417+yunusey@users.noreply.github.com>
2026-01-14 13:11:42 -0500

    Simulate a browser using chromium

--- README.md
+++ README.md
@@ -1,23 +1,40 @@
 # codeforces-extractor
-Blazingly fast codeforces problem extractor! *Rust btw!*
+Blazingly fast Codeforces test extractor! *Rust btw!*
 
 > [!CAUTION]
 > Before, the project was written in Python. Due to several reasons, it is now rewritten in Rust and is more performant and convenient to use.
 
-## Installation 📦
+## Update on January 14, 2026 (Please Read Carefully!)
+
+It appears that, recently, Codeforces has taken new measures to block bots trying to scrape data from the website. As a result of that, we have to simulate a browser--a very simple `curl` will not do the job.
+
+**Please note that this project's goal is to offer a better developer experience in *testing* and is only interested in, thus, scraping the test cases from the website. Please consult to Codeforces's policy statement for more information.**
+
+If you have been using `codeforces-extractor` directly or through [`codeforces-nvim`](https://github.com/yunusey/codeforces-nvim) for some time and noticed that the program isn't working correctly anymore, please update it by running `cargo install --git https://github.com/yunusey/codeforces-extractor`.
+
+## Installation
 You can install this tool using:
 ```bash
 cargo install --git https://github.com/yunusey/codeforces-extractor
 ```
 
-## Usage 📝
-Using this tool is pretty simple. The command takes two arguments:
+## Usage
+Using this tool is pretty simple. You can run `codeforces-extractor --help` to see the usage:
 ```bash
-codeforces-extractor <contest_id> --save-path <path>
-```
-You need to provide both the arguments (they do not have default values).
+Usage: codeforces-extractor [OPTIONS] <CONTEST_NAME>
 
-## Features 🚀
+Arguments:
+  <CONTEST_NAME>  Contest Name
+
+Options:
+  -s, --save-path <SAVE_PATH>  Save Path [default: ./problems]
+  -n, --native-display         Use native display (no Xvfb)
+  -h, --help                   Print help
+```
+
+Passing `--native-display` as an argument will launch `chromium` (or `google-chrome`) in your native display which will pop up a window. Though it works more reliably, most users would not, probably, prefer it. That's why, by default, `codeforces-extractor` will open up a virtual display instead using `Xvfb`. Again, my recommendation is, if it doesn't bother you, to use `--native-display` as it is a lot more reliable.
+
+## Features
 The tool will save the test cases like this:
 ```
  ./test
@@ -44,5 +61,9 @@ The tool will save the test cases like this:
    └──  0.out
 ```
 
-## Use with Neovim 🛠️
-The main reason for this tool is to be used with Neovim. You can check the plugin [here](https://github.com/yunusey/codeforces-nvim).
+## Use with Neovim
+This tool acts as the backend for my Neovim plugin, [codeforces-nvim](https://github.com/yunusey/codeforces-nvim), but can be used directly through the CLI, as described above.
+
+## Tests
+
+If you would like to run the tests yourself, please do not just run `cargo test` because there are, as of now, two tests fetching and parsing, and they cannot run simultaneously--I don't really know why though :D--that's why, you need to run them one by one using `cargo test -- --test-threads=1`.
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ mod parser;
 
 use std::fs;
 
+use chromiumoxide::browser::{Browser, BrowserConfig};
 use clap::Parser;
+use futures::StreamExt;
 use parser::{parse_content, Problem};
-use reqwest;
+
+use std::process::{Child, Command};
+use std::{env, thread, time::Duration};
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -14,18 +18,42 @@ struct Args {
     /// Save Path
     #[arg(short, long, default_value = "./problems")]
     save_path: std::path::PathBuf,
+
+    /// Use native display (no Xvfb)
+    #[arg(short, long, default_value_t = false)]
+    native_display: bool,
 }
 
-async fn get_content(contest_name: &str) -> String {
-    let client = reqwest::Client::new();
+fn start_xvfb() -> Child {
+    let display = ":99";
+    let child = Command::new("Xvfb")
+        .args([display, "-screen", "0", "1280x800x24", "-nolisten", "tcp"])
+        .spawn()
+        .expect("Failed to start Xvfb");
+
+    thread::sleep(Duration::from_millis(500));
+    env::set_var("DISPLAY", display);
+    child
+}
+
+async fn get_content(
+    contest_name: &str,
+    force_x11: bool,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let mut config_builder = BrowserConfig::builder().with_head();
+    if force_x11 {
+        config_builder = config_builder.arg("--ozone-platform=x11") // Ensure X11 platform is used for Xvfb
+    }
+    let config = config_builder.build()?;
+
+    let (browser, mut handler) = Browser::launch(config).await?;
+    tokio::spawn(async move { while let Some(_) = handler.next().await {} });
+
     let url = format!("https://codeforces.com/contest/{}/problems", contest_name);
-    let res = client
-        .get(&url)
-        .header("User-Agent", "Mozilla/5.0") // :D
-        .send()
-        .await
-        .unwrap();
-    res.text().await.unwrap()
+    let page = browser.new_page(&url).await?;
+    let html = page.content().await?;
+
+    Ok(html)
 }
 
 /// Save given `problem` to the given directory `save_path`
@@ -49,17 +77,28 @@ fn save_problem(problem: &Problem, save_path: &std::path::Path) {
 async fn main() {
     let args = Args::parse();
 
-    let content = get_content(&args.contest_name).await;
-    let problems: Vec<Problem> = parse_content(&content);
+    let xvfb_child = if args.native_display {
+        None
+    } else {
+        Some(start_xvfb())
+    };
+    let content = get_content(&args.contest_name, !args.native_display)
+        .await
+        .unwrap_or_else(|_| {
+            panic!("Failed to get content for contest: {}", &args.contest_name);
+        });
 
+    let problems: Vec<Problem> = parse_content(&content);
     for problem in problems {
         save_problem(&problem, &args.save_path);
     }
+
+    drop(xvfb_child);
 }
 
 #[tokio::test]
-async fn test_parser() {
-    let content = get_content("1790").await;
+async fn test_parser_1790() {
+    let content = get_content("1790", false).await.unwrap();
     let problems: Vec<Problem> = parse_content(&content);
     assert_eq!(
         problems
@@ -74,6 +113,27 @@ async fn test_parser() {
             "E. Vlad and a Pair of Numbers".to_string(),
             "F. Timofey and Black-White Tree".to_string(),
             "G. Tokens on Graph".to_string(),
+        ]
+    );
+}
+
+#[tokio::test]
+async fn test_parser_2184() {
+    let content = get_content("2184", false).await.unwrap();
+    let problems: Vec<Problem> = parse_content(&content);
+    assert_eq!(
+        problems
+            .iter()
+            .map(|p: &Problem| p.name.clone())
+            .collect::<Vec<String>>(),
+        vec![
+            "A. Social Experiment".to_string(),
+            "B. Hourglass".to_string(),
+            "C. Huge Pile".to_string(),
+            "D. Unfair Game".to_string(),
+            "E. Exquisite Array".to_string(),
+            "F. Cherry Tree".to_string(),
+            "G. Nastiness of Segments".to_string(),
         ]
     );
 }
